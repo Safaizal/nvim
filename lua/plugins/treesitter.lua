e66d1dad4ad70bbaff72ec82c36a375079c385f0
Safaizal <safaizalways@gmail.com>
2026-08-06 19:22:38 +0530

    fix: make it clean

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,28 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
-  build = ':TSUpdate'
+  build = ':TSUpdate',
+
+  config = function ()
+    local tree_config = require('nvim-treesitter')
+      tree_config.setup {
+        -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+        install_dir = vim.fn.stdpath('data') .. '/site'
+      }
+
+      tree_config.install{
+        'rust', 
+        'javascript', 
+        'zig',
+        'lua',
+        'python'
+      }
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { '<filetype>' },
+        callback = function() 
+          vim.treesitter.start() 
+        end,
+      })
+    end
 }
