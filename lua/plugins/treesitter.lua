0c27eafc6ad7896d8a5de7620c5745970b04b3aa
Safaizal <safaizalways@gmail.com>
2026-09-06 20:50:52 +0530

    fix: fixed some typos and unwanted lines

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,19 +3,12 @@ return {
   lazy = false,
   build = ':TSUpdate',
 
-  config = function ()
-    local tree_config = require('nvim-treesitter')
-      tree_config.setup {
-        -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
-        install_dir = vim.fn.stdpath('data') .. '/site',
-        auto_install = true,
-        indent = true,
-      }
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = { '<filetype>' },
-        callback = function()
-          vim.treesitter.start()
-        end,
-      })
-    end
+  config = function()
+    require("nvim-treesitter").setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+      auto_install = true,
+      indent = { enable = true },
+      highlight = { enable = true },
+    })
+  end,
 }
